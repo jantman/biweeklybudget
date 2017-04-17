697f7b034de7b20118854d06c3a80f598497e362
Jason Antman <jason@jasonantman.com>
2017-04-17 18:40:02 -0400

    issue #20 - sample data for reconcile tests

--- biweeklybudget/tests/acceptance/flaskapp/views/test_reconcile.py
+++ biweeklybudget/tests/acceptance/flaskapp/views/test_reconcile.py
@@ -36,12 +36,16 @@ Jason Antman <jason@jasonantman.com> <http://www.jasonantman.com>
 """
 
 import pytest
-from datetime import timedelta, date
-from selenium.webdriver.support.ui import Select
+from datetime import timedelta, datetime, date
+from pytz import UTC
 
 from biweeklybudget.utils import dtnow
 from biweeklybudget.tests.acceptance_helpers import AcceptanceHelper
+from biweeklybudget.models import *
+import biweeklybudget.models.base  # noqa
+from biweeklybudget.tests.conftest import engine
 
+dnow = dtnow()
 
 @pytest.mark.acceptance
 class TestReconcile(AcceptanceHelper):
@@ -65,3 +69,227 @@ class TestReconcile(AcceptanceHelper):
         div = selenium.find_element_by_id('notifications-row')
         assert div is not None
         assert div.get_attribute('class') == 'row'
+
+
+@pytest.mark.acceptance
+@pytest.mark.usefixtures('class_refresh_db', 'refreshdb')
+class TestReconcileSimple(AcceptanceHelper):
+
+    def test_00_clean_db(self, testdb):
+        # clean the database
+        biweeklybudget.models.base.Base.metadata.reflect(engine)
+        biweeklybudget.models.base.Base.metadata.drop_all(engine)
+        biweeklybudget.models.base.Base.metadata.create_all(engine)
+
+    def test_01_add_accounts(self, testdb):
+        a = Account(
+            description='First Bank Account',
+            name='BankOne',
+            ofx_cat_memo_to_name=True,
+            ofxgetter_config_json='{"foo": "bar"}',
+            vault_creds_path='secret/foo/bar/BankOne',
+            acct_type=AcctType.Bank
+        )
+        testdb.add(a)
+        a.set_balance(
+            overall_date=datetime(2017, 4, 10, 12, 0, 0, tzinfo=UTC),
+            ledger=1.0,
+            ledger_date=datetime(2017, 4, 10, 12, 0, 0, tzinfo=UTC)
+        )
+        b = Account(
+            description='Second Bank Account',
+            name='BankTwo',
+            acct_type=AcctType.Bank
+        )
+        testdb.add(b)
+        b.set_balance(
+            overall_date=datetime(2017, 4, 10, 12, 0, 0, tzinfo=UTC),
+            ledger=1.0,
+            ledger_date=datetime(2017, 4, 10, 12, 0, 0, tzinfo=UTC)
+        )
+        testdb.flush()
+        testdb.commit()
+
+    def test_02_add_budgets(self, testdb):
+        testdb.add(Budget(
+            name='1Income',
+            is_periodic=True,
+            description='1Income',
+            starting_balance=0.0,
+            is_income=True
+        ))
+        testdb.add(Budget(
+            name='2Periodic',
+            is_periodic=True,
+            description='2Periodic',
+            starting_balance=500.00
+        ))
+        testdb.add(Budget(
+            name='3Periodic',
+            is_periodic=True,
+            description='3Periodic',
+            starting_balance=0.00
+        ))
+        testdb.flush()
+        testdb.commit()
+
+    def test_03_add_transactions(self, testdb):
+        acct1 = testdb.query(Account).get(1)
+        acct2 = testdb.query(Account).get(2)
+        ibudget = testdb.query(Budget).get(1)
+        e1budget = testdb.query(Budget).get(2)
+        e2budget = testdb.query(Budget).get(3)
+        # income
+        testdb.add(Transaction(
+            date=date(2017, 4, 10),
+            actual_amount=-100.00,
+            budgeted_amount=-100.00,
+            description='income',
+            account=acct1,
+            budget=ibudget
+        ))
+        # one transaction
+        testdb.add(Transaction(
+            date=date(2017, 4, 10),
+            actual_amount=250.00,
+            description='trans1',
+            account=acct1,
+            budget=e2budget
+        ))
+        # another transaction
+        st1 = ScheduledTransaction(
+            amount=500.0,
+            description='ST1',
+            account=acct2,
+            budget=e1budget,
+            date=date(2017, 4, 10)
+        )
+        testdb.add(st1)
+        testdb.add(Transaction(
+            date=date(2017, 4, 11),
+            actual_amount=600.0,
+            budgeted_amount=500.0,
+            description='trans2',
+            account=acct2,
+            budget=e1budget,
+            scheduled_trans=st1
+        ))
+        # non-matched transaction
+        testdb.add(Transaction(
+            date=date(2017, 4, 14),
+            actual_amount=1400.00,
+            description='trans3',
+            account=acct2,
+            budget=e2budget
+        ))
+        # matched ScheduledTransaction
+        st2 = ScheduledTransaction(
+            amount=10.0,
+            description='ST2',
+            account=acct1,
+            budget=e2budget,
+            day_of_month=13
+        )
+        testdb.add(st2)
+        testdb.flush()
+        testdb.commit()
+
+    def test_04_add_ofx(self, testdb):
+        acct1 = testdb.query(Account).get(1)
+        acct2 = testdb.query(Account).get(2)
+        stmt1 = OFXStatement(
+            account=acct1,
+            filename='a1.ofx',
+            file_mtime=dnow,
+            as_of=dnow,
+            currency='USD',
+            acctid='1',
+            bankid='b1',
+            routing_number='r1'
+        )
+        testdb.add(stmt1)
+        stmt2 = OFXStatement(
+            account=acct2,
+            filename='a2.ofx',
+            file_mtime=dnow,
+            as_of=dnow,
+            currency='USD',
+            acctid='2',
+            bankid='b2',
+            routing_number='r2'
+        )
+        testdb.add(stmt2)
+        # transactions
+        testdb.add(OFXTransaction(
+            account=acct1,
+            statement=stmt1,
+            fitid='OFX1',
+            trans_type='Deposit',
+            date_posted=datetime(2017, 4, 10, 12, 3, 4, tzinfo=UTC),
+            amount=100.0,
+            name='ofx1-income'
+        ))
+        testdb.add(OFXTransaction(
+            account=acct1,
+            statement=stmt1,
+            fitid='OFX2',
+            trans_type='Debit',
+            date_posted=datetime(2017, 4, 11, 12, 3, 4, tzinfo=UTC),
+            amount=-250.0,
+            name='ofx2-trans1'
+        ))
+        testdb.add(OFXTransaction(
+            account=acct2,
+            statement=stmt2,
+            fitid='OFX3',
+            trans_type='Purchase',
+            date_posted=datetime(2017, 4, 9, 12, 3, 4, tzinfo=UTC),
+            amount=-600.0,
+            name='ofx3-trans2-st1'
+        ))
+        testdb.add(OFXTransaction(
+            account=acct1,
+            statement=stmt1,
+            fitid='OFXT4',
+            trans_type='Purchase',
+            date_posted=datetime(2017, 4, 14, 12, 3, 4, tzinfo=UTC),
+            amount=-10.0,
+            name='ofx4-st2'
+        ))
+        testdb.add(OFXTransaction(
+            account=acct2,
+            statement=stmt2,
+            fitid='OFXT5',
+            trans_type='Foo',
+            date_posted=datetime(2017, 4, 16, 12, 3, 4, tzinfo=UTC),
+            amount=123.0,
+            name='ofx5'
+        ))
+        testdb.flush()
+        testdb.commit()
+
+    def test_05_add_reconciled(self, testdb):
+        acct2 = testdb.query(Account).get(2)
+        stmt2 = testdb.query(OFXStatement).get(2)
+        e2budget = testdb.query(Budget).get(3)
+        o = OFXTransaction(
+            account=acct2,
+            statement=stmt2,
+            fitid='OFX6',
+            trans_type='Purchase',
+            date_posted=datetime(2017, 4, 17, 12, 3, 4, tzinfo=UTC),
+            amount=-600.0,
+            name='ofx6-trans4'
+        )
+        testdb.add(o)
+        t = Transaction(
+            date=date(2017, 4, 16),
+            actual_amount=600.00,
+            description='trans4',
+            account=acct2,
+            budget=e2budget
+        )
+        testdb.add(t)
+        testdb.add(TxnReconcile(transaction=t, ofx_trans=o))
+        testdb.flush()
+        testdb.commit()
