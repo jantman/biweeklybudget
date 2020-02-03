cadcf5875362e9b42111f1d97328f49a2a8d1740
Jason Antman <jason@jasonantman.com>
2020-02-03 06:39:43 -0500

    add back plaid_test.js - accidentally removed

---
+++ biweeklybudget/flaskapp/static/js/plaid_test.js
@@ -0,0 +1,33 @@
+/**
+ * TEST FUNCTION - Initiate a Plaid link. This function is for testing only; it
+ * directly POSTs to /ajax/plaid/get_access_token with
+ *
+ */
+function plaidLink() {
+    $.post('/ajax/plaid/get_access_token', {
+        public_token: 'CItest'
+    }, function(data) {
+        console.log("get_access_token response: %o", data);
+        $('#plaid_item_id').val(data.item_id);
+        $('#plaid_token').val(data.access_token);
+    }).fail(function() {
+        alert("ERROR: /ajax/plaid/get_access_token callback failed; see server log for details.")
+    });
+}
+
+/**
+ * TEST FUNCTION - Update a Plaid link. This function is for testing only!
+ */
+function plaidUpdate() {
+    console.log("called plaidUpdate()");
+    $.post('/ajax/plaid/create_public_token',
+        {access_token: $('#plaid_token').val()},
+        function(data) {
+            console.log("Got create_public_token response: %o", data);
+            $('#plaid_item_id').val("updatedItem-" + data.public_token);
+            $('#plaid_token').val("updatedToken-" + data.public_token);
+        },
+    ).fail(function() {
+        alert("ERROR: /ajax/plaid/create_public_token callback failed; see server log for details.")
+    });
+}
