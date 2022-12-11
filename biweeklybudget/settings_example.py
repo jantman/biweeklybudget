dc13be727738325385b6e293df5ef8c0a168f3fe
Jason Antman <jason@jasonantman.com>
2022-12-11 09:21:28 -0500

    another test fix

--- biweeklybudget/settings_example.py
+++ biweeklybudget/settings_example.py
@@ -83,18 +83,18 @@ DEFAULT_ACCOUNT_ID = 1
 FUEL_BUDGET_ID = 1
 
 #: Plaid Client ID
-PLAID_CLIENT_ID = os.environ['PLAID_CLIENT_ID']
+PLAID_CLIENT_ID = os.environ.get('PLAID_CLIENT_ID')
 
 #: Plaid Secret (client secret)
-PLAID_SECRET = os.environ['PLAID_SECRET']
+PLAID_SECRET = os.environ.get('PLAID_SECRET')
 
 #: Plaid Public Key
-PLAID_PUBLIC_KEY = os.environ['PLAID_PUBLIC_KEY']
+PLAID_PUBLIC_KEY = os.environ.get('PLAID_PUBLIC_KEY')
 
 #: Plaid environment name. Use 'sandbox' to test with Plaid's Sandbox
 #: environment (username: user_good, password: pass_good). Use `development` to
 #: test with live users and credentials and `production` to go live
-PLAID_ENV = os.environ['PLAID_ENV']
+PLAID_ENV = os.environ.get('PLAID_ENV')
 
 #: PLAID_PRODUCTS is a comma-separated list of products to use when initializing
 #: Link. Note that this list must contain 'assets' in order for the app to be
@@ -103,4 +103,4 @@ PLAID_PRODUCTS = 'transactions'
 
 #: PLAID_COUNTRY_CODES is a comma-separated list of countries for which users
 #: will be able to select institutions from.
-PLAID_COUNTRY_CODES = os.environ['PLAID_COUNTRY_CODES']
+PLAID_COUNTRY_CODES = os.environ.get('PLAID_COUNTRY_CODES')
