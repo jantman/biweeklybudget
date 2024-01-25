371fe5be5538255f13a28cd4bb27a79ccebbdd65
Jason Antman <jason@jasonantman.com>
2024-01-25 10:32:55 -0500

    fix version file

--- biweeklybudget/version.py
+++ biweeklybudget/version.py
@@ -35,5 +35,5 @@ Jason Antman <jason@jasonantman.com> <http://www.jasonantman.com>
 ################################################################################
 """
 
-VERSION = '1.1.1'
+VERSION = '1.2.0'
 PROJECT_URL = 'https://github.com/jantman/biweeklybudget'
